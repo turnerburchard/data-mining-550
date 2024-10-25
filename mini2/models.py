0484b69b94f0303fc6331e7645a4ee27158b2164
turnerburchard <tburchard4@gmail.com>
2024-10-25 11:59:24 -0600

    model training

--- mini2/models.py
+++ mini2/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.feature_selection import SelectKBest
 import preprocessing
 
 df = preprocessing.preprocess()
@@ -19,7 +20,34 @@ def simple_regression(X, y):
     model.fit(X, y)
     return model
 
-model = simple_regression(df[['Estimate (Land)']], df['Sale Price'])
+simple_model = simple_regression(df[['Estimate (Land)']], df['Sale Price'])
 
 print("Simple Regression Error: ")
-print(cross_validate(model, df[['Estimate (Land)']], df['Sale Price']))
+print(cross_validate(simple_model, df[['Estimate (Land)']], df['Sale Price']))
+
+
+# Multiple Regression with all features
+
+def multiple_regression(X, y):
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
+
+multiple_model = multiple_regression(df.drop(columns=['Sale Price']), df['Sale Price'])
+
+print("Multiple Regression Error: ")
+print(cross_validate(multiple_model, df.drop(columns=['Sale Price']), df['Sale Price']))
+
+
+def subset_selection(X, y, k):
+    selector = SelectKBest(k=k)
+    return selector.fit_transform(X, y)
+
+
+# Multiple Regression with subset of features
+k = 15
+X = subset_selection(df.drop(columns=['Sale Price']), df['Sale Price'], k)
+subset_model = multiple_regression(X, df['Sale Price'])
+
+print("Subset Regression Error: ")
+print(cross_validate(subset_model, X, df['Sale Price']))
--- mini2/preprocessing.py
+++ mini2/preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 def preprocess():
     df = pd.read_csv("cook_county_train_val.csv")
     df = df[df['Sale Price'] > 1]
-    df = df.drop(columns=['PIN', 'Deed No.'])
+    df = df.drop(columns=['PIN', 'Deed No.', 'Modeling Group'])
     df = df.drop(columns=['Description'])
 
     return df
