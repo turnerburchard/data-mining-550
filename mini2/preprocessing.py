81a2fcf08d5c4825a81f2444ce3e545ea8258d11
turnerburchard <tburchard4@gmail.com>
2024-11-04 14:30:57 -0700

    update preprocesing

--- mini2/preprocessing.py
+++ mini2/preprocessing.py
@@ -4,8 +4,15 @@ import numpy as np
 
 def preprocess():
     df = pd.read_csv("cook_county_train_val.csv")
+    # drop rows with missing sale price
     df = df[df['Sale Price'] > 1]
-    df = df.drop(columns=['PIN', 'Deed No.', 'Modeling Group'])
-    df = df.drop(columns=['Description'])
+    # drop categorical variables
+    # could regex description for more info
+    df = df.drop(columns=['PIN', 'Deed No.', 'Modeling Group', 'Description'])
+
+    # drop empty / mostly empty columns
+    # could drop "Multi Property Indicator" as well
+    df = df.drop(columns=['Apartments', 'Garage 2 Material', 'Garage 2 Attachment', 'Garage 2 Area',
+                          'Number of Commercial Units', 'Use'])
 
     return df
