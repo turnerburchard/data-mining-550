00e68465aaf27f2e78a0ab2cc33400e8c248dffe
turnerburchard <tburchard4@gmail.com>
2024-11-06 11:16:03 -0700

    update preprocesing

--- mini2/preprocessing.py
+++ mini2/preprocessing.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def preprocess():
-    df = pd.read_csv("mini2\cook_county_train_val.csv")
+    df = pd.read_csv("cook_county_train_val.csv")
     # drop rows with missing sale price
     df = df[df['Sale Price'] > 1]
     # drop categorical variables
@@ -14,5 +14,12 @@ def preprocess():
     # could drop "Multi Property Indicator" as well
     df = df.drop(columns=['Apartments', 'Garage 2 Material', 'Garage 2 Attachment', 'Garage 2 Area',
                           'Number of Commercial Units', 'Use'])
+
+    # remove outliers with Isolation Forest
+    from sklearn.ensemble import IsolationForest
+    iso = IsolationForest(contamination=0.05)
+    yhat = iso.fit_predict(df[['Sale Price']])
+    mask = yhat != -1
+    df = df[mask]
 
     return df
